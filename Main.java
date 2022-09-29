a71731b54a615622df87be3378563674a7af5b73
Nayia Nicolaides <112590488+NayiaNicolaides@users.noreply.github.com>
2022-09-29 00:59:56 +0000

    Finding the length of a word

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
     Scanner myScanner = new Scanner(System.in);
     String UserInput = myScanner.nextLine();
     int count = UserInput.length();
     System.out.println(count);
+    myScanner.close();
   }
 }
